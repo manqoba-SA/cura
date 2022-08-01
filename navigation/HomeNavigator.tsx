b188f83e54ac6ed42361242dc1d63e2d53a5a0e4
Manqoba <nqobi.it4073@gmail.com>
2022-08-01 23:13:37 +0200

    Make separate component for customHeader

--- navigation/HomeNavigator.tsx
+++ navigation/HomeNavigator.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/homeScreens.jsx/Home";
 import Settings from "../screens/SettingsScreen";
+import CustomHeader from "../components/CustomHeader/CustomHeader";
 
 export default function HomeNavigator() {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen component={Home} name="home" />
+      <Stack.Screen
+        component={Home}
+        options={{
+          headerShown: true,
+          header: (props) => <CustomHeader navigation={""} {...props} />,
+        }}
+        name="home"
+      />
       <Stack.Screen component={Settings} name="settings" />
     </Stack.Navigator>
   );
--- screens/homeScreens.jsx/Home.tsx
+++ screens/homeScreens.jsx/Home.tsx
@@ -6,44 +6,5 @@ import settingsIcon from "@iconify/icons-arcticons/settings";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function Home({ navigation }) {
-  return (
-    <View style={{ backgroundColor: "#E5E5E5", flex: 1.0 }}>
-      <View
-        style={{
-          backgroundColor: "#fff",
-          height: "15%",
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            marginTop: 10,
-          }}
-        >
-          <View style={{ flex: 4.0, marginHorizontal: 15 }}>
-            <Text
-              style={{
-                fontWeight: "bold",
-                fontSize: 20,
-                color: COLORS.primary.text,
-                fontFamily: "Poppins_400Regular",
-              }}
-            >
-              Hello Oratile
-            </Text>
-          </View>
-          <View style={{ flex: 1.0 }}>
-            <TouchableOpacity onPress={() => navigation.navigate("settings")}>
-              <Ionicons name="settings-outline" size={35} color="#A1A3AB" />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
+  return <View style={{ backgroundColor: "#E5E5E5", flex: 1.0 }}></View>;
 }
