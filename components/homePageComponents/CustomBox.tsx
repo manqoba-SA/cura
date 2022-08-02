af9cb0dcfe0a07cf21e0151b2be9a258fdf8dcb0
Manqoba <nqobi.it4073@gmail.com>
2022-08-02 21:43:25 +0200

    make home screen ui

---
+++ components/homePageComponents/CustomBox.tsx
@@ -0,0 +1,83 @@
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import React from "react";
+import COLORS from "../../constants/COLORS";
+
+export default function CustomBox({ title, subtitle, type = "full" }) {
+  return (
+    <View style={[styles.box, styles[`${type}_box`]]}>
+      {type === "medium" ? (
+        <View style={{ flex: 1.0, marginTop: 20 }}>
+          <Text style={[styles.boxTitle, styles[`${type}_boxTitle`]]}>
+            {title}
+          </Text>
+          <Text style={[styles.boxSubTitle, styles[`${type}_boxSubTitle`]]}>
+            {subtitle}
+          </Text>
+        </View>
+      ) : (
+        <>
+          <Image
+            source={require("../../../cura/assets/images/illustrations/undraw_doctors_hwty.png")}
+            style={[styles.boxImage, styles[`${type}_boxImage`]]}
+          />
+          {type === "full" ? (
+            <View style={{ flex: 1.0 }}>
+              <Text style={styles.boxTitle}>{title}</Text>
+              <Text style={styles.boxSubTitle}>{subtitle}</Text>
+            </View>
+          ) : (
+            <Text style={styles.boxTitle}>{title}</Text>
+          )}
+        </>
+      )}
+    </View>
+  );
+}
+const { width, height } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#fff",
+    height: height * 0.21,
+    borderRadius: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  full_box: {
+    flexDirection: "row",
+    margin: 20,
+  },
+  half_box: {
+    marginHorizontal: 20,
+    width: "50%",
+  },
+  medium_box: {
+    backgroundColor: COLORS.primary.text,
+    width: "80%",
+    marginTop: 12,
+  },
+  boxImage: {
+    resizeMode: "contain",
+  },
+  full_boxImage: {
+    flex: 1.0,
+    height: "70%",
+  },
+  half_boxImage: {
+    height: "50%",
+  },
+  boxTitle: {
+    color: COLORS.primary.text,
+    fontFamily: "Poppins_600SemiBold",
+    fontSize: 20,
+  },
+  medium_boxTitle: {
+    color: "#000",
+  },
+  boxSubTitle: {
+    fontFamily: "Poppins_400Regular",
+    color: "grey",
+  },
+  medium_boxSubTitle: {
+    color: "#000",
+  },
+});
--- screens/homeScreens.jsx/Home.tsx
+++ screens/homeScreens.jsx/Home.tsx
@@ -1,10 +1,39 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
 import COLORS from "../../constants/COLORS";
 import { Icon } from "@iconify/react";
 import settingsIcon from "@iconify/icons-arcticons/settings";
 import { Ionicons } from "@expo/vector-icons";
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
+import CustomBox from "../../components/homePageComponents/CustomBox";
 
 export default function Home({ navigation }) {
-  return <View style={{ backgroundColor: "#E5E5E5", flex: 1.0 }}></View>;
+  const { width, height } = Dimensions.get("window");
+  return (
+    <View style={{ backgroundColor: "#E5E5E5", flex: 1.0 }}>
+      <View style={{ flex: 1.0 }}>
+        <CustomBox
+          title={"Diagnose"}
+          subtitle="Hi Oratile, I can help you learn more about your sysmptoms"
+        />
+        <View style={{ flexDirection: "row", width: width * 0.8, margin: 0 }}>
+          <CustomBox title={"Health\nLibrary"} type="half" />
+          <CustomBox title={"Medicine Reminder"} type="half" />
+        </View>
+        <View style={{ margin: 20 }}>
+          <Text style={{ fontFamily: "Poppins_400Regular", fontSize: 20 }}>
+            Check latest Health Articles
+          </Text>
+          <CustomBox
+            title={"Sleep meditation"}
+            subtitle="Heaalth is very important in life because when health is a alive.."
+            type="medium"
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
