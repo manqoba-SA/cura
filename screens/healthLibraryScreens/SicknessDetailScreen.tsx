37323ee48ade21d00d1bed6e1e2fff4270a1fd7b
Manqoba <nqobi.it4073@gmail.com>
2022-08-07 17:59:27 +0200

    Make Expandable View Component

--- screens/healthLibraryScreens/SicknessDetailScreen.tsx
+++ screens/healthLibraryScreens/SicknessDetailScreen.tsx
@@ -1,10 +1,179 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Platform,
+  UIManager,
+  LayoutAnimation,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Poppins_600SemiBold } from "@expo-google-fonts/poppins";
+
+const ExpandableComponent = ({ item, onClickFunction }) => {
+  //Custom Component for the Expandable List
+  const [layoutHeight, setLayoutHeight] = useState(0);
+
+  useEffect(() => {
+    if (item.isExpanded) {
+      setLayoutHeight(null);
+    } else {
+      setLayoutHeight(0);
+    }
+  }, [item.isExpanded]);
+
+  return (
+    <View style={{ marginVertical: 15, marginHorizontal: 10 }}>
+      {/*Header of the Expandable List Item*/}
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={onClickFunction}
+        style={styles.header}
+      >
+        <Text style={styles.headerText}>{item.category_name}</Text>
+      </TouchableOpacity>
+      <View
+        style={{
+          height: layoutHeight,
+          overflow: "hidden",
+        }}
+      >
+        {/*Content under the header of the Expandable List Item*/}
+        {item.subcategory.map((item, key) => (
+          <TouchableOpacity
+            key={key}
+            style={styles.content}
+            onPress={() => alert("Id: " + item.id + " val: " + item.val)}
+          >
+            <Text style={styles.text}>
+              {key}. {item.val}
+            </Text>
+            <View style={styles.separator} />
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+};
 
 export default function SicknessDetailScreen() {
+  const [listDataSource, setListDataSource] = useState(CONTENT);
+  const [multiSelect, setMultiSelect] = useState(false);
+
+  if (Platform.OS === "android") {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+
+  const updateLayout = (index) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    const array = [...listDataSource];
+    if (multiSelect) {
+      // If multiple select is enabled
+      array[index]["isExpanded"] = !array[index]["isExpanded"];
+    } else {
+      // If single select is enabled
+      array.map((value, placeindex) =>
+        placeindex === index
+          ? (array[placeindex]["isExpanded"] = !array[placeindex]["isExpanded"])
+          : (array[placeindex]["isExpanded"] = false)
+      );
+    }
+    setListDataSource(array);
+  };
   return (
     <View style={{ backgroundColor: "#E5E5E5", flex: 1.0 }}>
-      <Text>SicknessDetailScreen</Text>
+      <View
+        style={{ flex: 1.0, alignItems: "center", justifyContent: "center" }}
+      >
+        <Text style={{ fontSize: 24, fontFamily: "Poppins_600SemiBold" }}>
+          Overview
+        </Text>
+        <Text style={{ color: "#979797", fontSize: 15 }}>
+          A spectacular visual journey through 40 years of haute couture from
+          one of the best-known and most trend-setting brands in fashion.A
+          spectacular visual journey through 40 years of haute couture from one
+          of the best-known and most trend-setting brands in fashion.
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 2.0,
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <ScrollView
+          style={{
+            width: "100%",
+          }}
+        >
+          {listDataSource.map((item, key) => (
+            <ExpandableComponent
+              key={item.category_name}
+              onClickFunction={() => {
+                updateLayout(key);
+              }}
+              item={item}
+            />
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleText: {
+    flex: 1,
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  header: {
+    backgroundColor: "#F5FCFF",
+    padding: 20,
+  },
+  headerText: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  separator: {
+    height: 0.5,
+    backgroundColor: "#808080",
+    width: "95%",
+    marginLeft: 16,
+    marginRight: 16,
+  },
+  text: {
+    fontSize: 16,
+    color: "#606070",
+    padding: 10,
+  },
+  content: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: "#fff",
+  },
+});
+
+const CONTENT = [
+  {
+    isExpanded: false,
+    category_name: "Item 1",
+    subcategory: [
+      { id: 1, val: "Sub Cat 1" },
+      { id: 3, val: "Sub Cat 3" },
+    ],
+  },
+  {
+    isExpanded: false,
+    category_name: "Item 2",
+    subcategory: [
+      { id: 4, val: "Sub Cat 4" },
+      { id: 5, val: "Sub Cat 5" },
+    ],
+  },
+];
