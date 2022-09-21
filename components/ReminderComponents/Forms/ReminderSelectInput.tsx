41466b94fd6d65b7f623cfb236446fd6b0c1a66b
Manqoba <nqobi.it4073@gmail.com>
2022-09-21 21:12:30 +0200

    Fix Reminder Screen

--- components/ReminderComponents/Forms/ReminderSelectInput.tsx
+++ components/ReminderComponents/Forms/ReminderSelectInput.tsx
@@ -1,34 +1,80 @@
 import { View, Text } from "react-native";
 import React, { Fragment } from "react";
-import { CheckIcon, Select } from "native-base";
+import SelectDropdown from "react-native-select-dropdown";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function ReminderSelectInput({
   medicineType,
   onValueChange,
   type = "",
   placeholder,
-  chiildren,
 }) {
+  const howMany = [
+    "Once A Day",
+    "Twice",
+    "Three, ",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+  ];
+  const medicineTypes = ["Tablet", "Canada", "Australia", "Ireland"];
   return (
-    <Select
-      bgColor={"tertiary.50"}
-      borderBottomLeftRadius={type === "first" ? 14 : 0}
-      borderBottomRightRadius={type === "first" ? 0 : 14}
-      borderColor="primary.50"
-      paddingY={10}
-      borderWidth={0.5}
-      selectedValue={medicineType}
-      minWidth="50%"
-      accessibilityLabel="Choose Medicine Type"
-      placeholder={placeholder}
-      _selectedItem={{
-        bg: "primary.50",
-        endIcon: <CheckIcon size="5" />,
+    // <Select
+    //   bgColor={"tertiary.50"}
+    //   borderBottomLeftRadius={type === "first" ? 14 : 0}
+    //   borderBottomRightRadius={type === "first" ? 0 : 14}
+    //   borderColor="primary.50"
+    //   paddingY={10}
+    //   borderWidth={0.5}
+    //   selectedValue={medicineType}
+    //   minWidth="50%"
+    //   accessibilityLabel="Choose Medicine Type"
+    //   placeholder={placeholder}
+    //   _selectedItem={{
+    //     bg: "primary.50",
+    //     endIcon: <CheckIcon size="5" />,
+    //   }}
+    //   onValueChange={onValueChange}
+    // >
+    //   <Select.Item label="Dose" value="dose" />
+    // </Select>
+
+    <SelectDropdown
+      data={type === "first" ? howMany : medicineTypes}
+      onSelect={(selectedItem, index) => {
+        console.log(selectedItem, index);
       }}
-      onValueChange={onValueChange}
-    >
-      <Select.Item label="Dose" value="dose" />
-      {chiildren}
-    </Select>
+      searchPlaceHolder="Search"
+      rowStyle={{ backgroundColor: "white" }}
+      buttonStyle={{
+        backgroundColor: "#F8F8F6",
+        width: "50%",
+        borderBottomLeftRadius: type === "first" ? 14 : 0,
+        borderBottomRightRadius: type === "first" ? 0 : 14,
+        borderWidth: 0.5,
+        borderColor: "#409849",
+      }}
+      renderDropdownIcon={(isOpened) => {
+        return (
+          <FontAwesome
+            name={isOpened ? "chevron-up" : "chevron-down"}
+            color={"#444"}
+            size={18}
+          />
+        );
+      }}
+      dropdownIconPosition={"right"}
+      buttonTextAfterSelection={(selectedItem, index) => {
+        // text represented after item is selected
+        // if data array is an array of objects then return selectedItem.property to render after item is selected
+        return selectedItem;
+      }}
+      rowTextForSelection={(item, index) => {
+        // text represented for each item in dropdown
+        // if data array is an array of objects then return item.property to represent item in dropdown
+        return item;
+      }}
+    />
   );
 }
--- constants/dummyData/remedies.ts
+++ constants/dummyData/remedies.ts
@@ -23,11 +23,12 @@ const remedies = [
   },
   {
     id: 2,
-    title: "Honey Tea",
-    description: "According to some research, honey may relieve coughs",
+    title: "Probiotics",
+    description:
+      "According to some research, Probiotics can help to relieve coughs",
     image: require("../../assets/images/honeyTea.jpg"),
     overview:
-      "Researchers in one 2007 studyTrusted Source found evidence to suggest that buckwheat honey may be more effective than traditional medication at relieving cough.",
+      "Probiotics are microorganisms that can provide a host of health benefits. While they don’t relieve a cough directly, they do help to balance your gastrointestinal flora. Gastrointestinal flora are the bacteria that live in your intestines.",
     ingrediens: [
       { id: 1, name: "Roibos Tea", image: "" },
       { id: 2, name: "Honey", image: "" },
@@ -37,11 +38,11 @@ const remedies = [
   },
   {
     id: 3,
-    title: "Honey Tea",
+    title: "Cut Up an Onion",
     description: "According to some research, honey may relieve coughs",
     image: require("../../assets/images/honeyTea.jpg"),
     overview:
-      "Researchers in one 2007 studyTrusted Source found evidence to suggest that buckwheat honey may be more effective than traditional medication at relieving cough.",
+      "Many people start to cry the minute they begin chopping up an onion. But while there isn’t any science behind it, some people swear that the strong vapor an onion emits when it’s cut helps relieve a cough, says Russell. Before you go to bed, cut an onion into quarters and leave it on a plate on your bedside table or at the foot of the bed. Although using onions may sound like nothing more than an old wives' tale, Russell says it's quite popular in Spain and France.",
     ingrediens: [
       { id: 1, name: "Roibos Tea", image: "" },
       { id: 2, name: "Honey", image: "" },
